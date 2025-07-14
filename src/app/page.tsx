e6f14487ff91b1a6684c672e1101220ed741ae7a
Shashi Bhushan Kumar <shashi.bkumar21@gmail.com>
2025-07-14 20:54:33 +0000

    please also make responsive after all are good

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -148,7 +148,7 @@ export default function Home() {
       
       <section className="py-20">
         <div className="container mx-auto px-4">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {featuredUniversities.map((uni, index) => (
               <UniversityCard key={index} {...uni} />
             ))}
