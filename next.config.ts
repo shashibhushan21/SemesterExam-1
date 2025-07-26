2c5f1fe04b96a0c4680842e147957d4b148156f7
Shashi Bhushan Kumar <shashi.bkumar21@gmail.com>
2025-07-26 14:36:18 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'tse4.mm.bing.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
