80be5ab6272575ae4e6e82c03f9e668b01158315
Shashi Bhushan Kumar <shashi.bkumar21@gmail.com>
2025-07-26 14:46:07 +0000

    Error: Invalid src prop (https://res.cloudinary.com/bhushancloud/image/u

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
